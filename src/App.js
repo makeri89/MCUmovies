eea8fbbcb3898a9d37ddcdafe770ecd20b87f95f
Markus Kaihola <makeri89@hotmail.com>
2020-10-31 00:27:00 +0200

    css responsiveness and remove useless stuff

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const App = () => {
   const [movies, setMovies] = useState([])
   const [searchState, setSearchState] = useState('')
   const [movieList, setMovieList] = useState([])
-  const [message, setMessage] = useState('Add a search argument')
   
   useEffect(() => {
     axios
@@ -26,29 +25,25 @@ const App = () => {
   const handleSearch = (e) => {
     e.preventDefault()
     setMovieList(movies.filter(movie => movie.release_date.startsWith(searchState)))
-    
-    if (searchState === '') {
-      setMessage('Add a search argument')
-    }
     setSearchState('')
   }
 
   return(
     <div>
       <div id='header'>
-        <h1 id='title'>List of MCU movies</h1>
+        <h1 id='title'>MCU movies</h1>
         <form id='search'>
           <h2>Search movies by year:</h2>
           <input type="text" onChange={handleSearchChange} value={searchState}/><br/>
-          {message} <br/>
+          <span>Add a search argument</span> <br/>
           <button onClick={handleSearch}>Search</button>
         </form>
       </div>
       {movieList.length === 0 ? 
-        <p>No movies found</p> :
+        <p id='nomovies'>No movies found from that year</p> :
         <div id='moviesListed'>
           {movieList.map(movie =>
-            <MovieForm key={movie.id} movie={movie} message={message} />  
+            <MovieForm key={movie.id} movie={movie}/>  
           )}
         </div>
       }
