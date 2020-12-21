475ced1a5d82097b58d102ed6c4ca665dda73e9e
Markus Kaihola <makeri89@hotmail.com>
2020-12-21 15:12:21 +0200

    small fix

--- cypress/integration/movies.js
+++ cypress/integration/movies.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 describe('Movies', function() {
   beforeEach(function() {
     cy.visit('http://localhost:3000')
@@ -8,10 +9,10 @@ describe('Movies', function() {
   })
 
   it('search works', function() {
-    cy.wait(5000)
+    cy.wait(1500)
     cy.get('input:first').type('2018')
     cy.get('#searchbutton').click()
-    cy.wait(5000)
+    cy.wait(1500)
     cy.contains('Black Panther')
     cy.contains('Iron Man').should('not.exist')
   })
